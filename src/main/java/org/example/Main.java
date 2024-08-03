68ca142c56af2217e27497aafef515ff9200a897
Dmitry Buldakov <dabuldakov@mail.ru>
2024-08-03 18:30:57 +0700

    zeroNumbersToEndSwap

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,8 @@
 package org.example;
 
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -8,13 +10,30 @@ public class Main {
         int[][] matrix2 = {{1, 3, 6}, {2, 6, 7}, {5, 7, 3}};
         int[][] matrix3 = {{1, 1, 1, 1}, {1, 1, 1, 1}, {1, 1, 1, 1}, {1, 1, 1, 1}};
 
+        int[] zeroNum = {0, 1, 0, 3, 12, 5, 0};
+        zeroNumbersToEndSwap(zeroNum);
+        System.out.println(Arrays.toString(zeroNum));
 
+    }
 
+    public static void zeroNumbersToEndSwap(int[] array) {
+        for (int j = 0; j < array.length; j++) {
+            for (int i = 0; i < array.length-1-j; i++) {
+                if (array[i] == 0 && array[i+1] !=0) {
+                    swap(array, i, i+1);
+                }
+            }
+        }
+    }
+
+    private static void swap(int[] array, int a, int b) {
+        int save = array[a];
+        array[a] = array[b];
+        array[b] = save;
     }
 
     public static int diagonalNumberSum(int[][] matrix) {
         int sum = 0;
-
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
                 if (i == j) {
@@ -23,9 +42,6 @@ public class Main {
                 }
             }
         }
-
         return sum;
     }
-
-
 }
